d9619779aa9e5e4982f02059ee992f017e04b2bf
goeun-oh <qw433812@gmail.com>
2025-06-03 19:54:30 +0900

    SMS-10 add config_rom

---
+++ SCCB_Controller.sv
@@ -0,0 +1,12 @@
+`timescale 1ns/1ps
+
+
+module SCCB_Controller(
+    input logic clk,
+    input logic reset,
+    output logic scl,
+    output logic sda
+);
+    typedef enum  { IDLE,  } name;
+
+endmodule
