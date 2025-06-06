2e6cfcd3d2ebb856630894026005fc78d5586a3e
goeun-oh <qw433812@gmail.com>
2025-06-06 18:45:59 +0900

    Add game controller, score calculator, and display modules
    - Implemented `ball_rom` to store pixel data for a 20x20 image.
    - Created `btn_debounce` module for button debouncing logic.
    - Developed `fnd_controller` for 7-segment display control.
    - Added `frame_buffer` for handling pixel data read/write operations.
    - Introduced `game_controller` to manage game state and ball movement.
    - Implemented `scoreText` module to display score on screen.
    - Created `score_calculator` to manage scoring logic based on game events.
    - Added `tick_400kHz` module for generating a 400kHz clock signal.

---
+++ I2C/I2C_Controller.sv
@@ -0,0 +1,170 @@
+`timescale 1ns / 1ps
+
+module I2C_Controller(
+    input  logic       clk,
+    input  logic       reset,
+    input  logic       ball_send_trigger,
+    input  logic [9:0] ball_y,
+    input  logic [7:0] ball_vy,
+    input  logic       ready,
+    output logic       start,
+    output logic       stop,
+    output logic       i2c_en,
+    output logic [7:0] tx_data,
+    input  logic       tx_done,
+    output logic       is_transfer
+);
+
+    typedef enum{
+        IDLE,
+        WAIT,
+        SEND_ADDR,
+        SEND_REG_ADDR,
+        SEND_DATA,
+        STOP,
+        DONE
+    } state_t;
+
+    state_t state, state_next;
+
+    logic [7:0]  slv0_data0, slv0_data1, slv1_data0;
+    logic [7:0] i2c_addr, tx_data_reg, tx_data_next;
+    
+    //state 관련//
+    logic [1:0] state_cnt_reg, state_cnt_next; 
+    //0: idle, 1: send addr, 2: send register addr, 3: send data
+    logic [1:0] state_addr_reg, state_addr_next;
+    logic [1:0] state_data_reg, state_data_next;
+
+    //i2c 주소관련//
+    assign i2c_addr = 8'haa;
+    assign slv0_addr = 8'h00;  //ball y pos
+    assign slv1_addr = 8'h01;  //ball y vel
+    
+    //i2c 전송 data 관련//
+    assign slv0_data0 = {ball_y[9:8], 6'b0};  // 공 y 좌표의 최상위 2비트
+    assign slv0_data1 = ball_y[7:0];  //공 y 좌표 나머지
+    assign slv1_data0 = ball_vy;  //공 속도
+    assign tx_data =tx_data_reg;
+
+    always_ff @(posedge clk or posedge reset) begin
+        if (reset) begin
+            state <= IDLE;
+            state_cnt_reg <=0;
+            state_addr_reg <=0;
+            state_data_reg <=0;
+            tx_data_reg <=0;
+        end else begin
+            state <= state_next;
+            state_cnt_reg <= state_cnt_next;
+            state_addr_reg <= state_addr_next;
+            state_data_reg <= state_data_next;
+            tx_data_reg <=tx_data_next;
+        end
+    end
+
+    // 기본값
+    always_comb begin
+        start         = 0;
+        stop          = 0;
+        i2c_en        = 0;
+        tx_data_next  = tx_data_reg;
+        is_transfer = 1;
+        state_next    = state;
+        state_cnt_next = state_cnt_reg;
+        state_addr_next = state_addr_reg;
+        state_data_next = state_data_reg;
+
+        case (state)
+            IDLE: begin
+                state_cnt_next =0;
+                state_addr_next =0;
+                state_data_next =0;
+                tx_data_next =0;
+                is_transfer = 0;
+                if (ball_send_trigger) begin
+                    state_next = WAIT;
+                end
+            end
+
+            WAIT: begin
+                if(ready) begin
+                    case(state_cnt_reg)
+                        2'd0: begin
+                            state_next = SEND_ADDR;
+                        end
+                        2'd1: begin
+                            state_next = SEND_REG_ADDR;
+                        end
+                        2'd2: begin
+                            state_next= SEND_DATA;
+                        end
+                        2'd3: begin
+                            state_next = STOP;
+                        end
+                    endcase
+                end
+            end
+
+            SEND_ADDR: begin
+                tx_data_next = i2c_addr;
+                i2c_en = 1;
+                state_next = WAIT;
+                state_cnt_next = state_cnt_reg +1;
+            end
+
+            SEND_REG_ADDR: begin
+                i2c_en = 1;
+                state_next = WAIT;
+                case(state_addr_reg)
+                    2'd0: begin
+                        tx_data_next = slv0_addr;
+                        state_cnt_next = state_cnt_reg +1;
+                    end
+                    2'd1: begin
+                        tx_data_next = slv1_addr;
+                        state_cnt_next = state_cnt_reg +1;
+                    end
+                endcase
+            end
+
+            SEND_DATA: begin
+                i2c_en = 1;
+                state_next =WAIT;
+                case (state_addr_reg)
+                    2'd0: begin
+                        case(state_data_reg)
+                            2'd0: begin
+                                tx_data_next = slv0_data0; 
+                                state_data_next = state_data_reg +1;
+                            end
+                            2'd1: begin
+                                tx_data_next = slv0_data1; 
+                                state_addr_next = state_addr_reg +1;
+                                state_data_next = 0;
+                                state_cnt_next = state_cnt_reg -1;
+                            end
+                        endcase
+                    end
+                    2'd1: begin
+                        tx_data_next = slv1_data0;
+                        state_addr_next =0;
+                        state_cnt_next = state_cnt_reg +1;
+                    end
+                endcase
+            end
+
+            STOP: begin
+                stop = 1;
+                i2c_en = 1;
+                state_next = DONE;
+            end
+
+            DONE: begin
+                is_transfer = 0;
+                state_next = IDLE;
+            end
+        endcase
+    end
+
+endmodule
---
+++ game_controller.sv
@@ -0,0 +1,191 @@
+`timescale 1ns / 1ps
+
+module game_controller (
+    input  logic       clk_25MHZ,
+    input  logic       reset,
+    input  logic       upscale,
+    input  logic       collision_detected,
+    output logic [9:0] ball_x_out,
+    output logic [9:0] ball_y_out,
+    output logic       is_ball_moving_left,
+    input  logic [9:0] estimated_speed,
+    input  logic       game_start,
+    output logic       game_over,
+    output logic [7:0] score_test,
+    
+    //상대 보드에 공 정보 전송 wire
+    output logic       ball_send_trigger,
+    output logic [7:0] ball_vy
+);
+
+    typedef enum logic [1:0] {
+        IDLE = 0,
+        RUNNING_RIGHT = 1,
+        RUNNING_LEFT = 2,
+        STOP = 3
+    } state_t;
+
+    state_t state, next;
+
+    logic [9:0] ball_x_next, ball_y_next;
+    logic signed [9:0] ball_y_vel, ball_y_vel_next;
+    logic ball_send_trigger_reg, ball_send_trigger_next;
+    logic [31:0] ball_counter, ball_counter_next;
+    logic [1:0] gravity_counter, gravity_counter_next;
+    logic [1:0] x_counter, x_counter_next;
+    logic [9:0] safe_speed;
+    // 속도 갱신용
+    logic [19:0] ball_speed, ball_speed_next;
+    logic [9:0] y_min = 0;
+    logic [9:0] y_max;
+
+    logic game_over_next;
+
+    logic [7:0] score_test_next;
+    
+    assign ball_send_trigger = ball_send_trigger_reg;
+    assign ball_vy = ball_y_vel;
+
+
+    always_ff @(posedge clk_25MHZ or posedge reset) begin
+        if (reset) begin
+            state <= IDLE;
+            ball_x_out <= 100;
+            ball_y_out <= 80;
+            ball_counter <= 0;
+            gravity_counter <= 0;
+            x_counter <= 0;
+            ball_y_vel <= -3;
+            ball_speed <= 20'd270000;
+            game_over <= 0;
+            score_test <= 0;
+            ball_send_trigger_reg <=0;
+        end else begin
+            state <= next;
+            ball_x_out <= ball_x_next;
+            ball_y_out <= ball_y_next;
+            ball_counter <= ball_counter_next;
+            gravity_counter <= gravity_counter_next;
+            x_counter <= x_counter_next;
+            ball_y_vel <= ball_y_vel_next;
+            ball_speed <= ball_speed_next;
+            game_over <= game_over_next;
+            score_test <= score_test_next;
+            ball_send_trigger_reg <= ball_send_trigger_next;
+        end
+    end
+
+    always_comb begin
+        next = state;
+        ball_x_next = ball_x_out;
+        ball_y_next = ball_y_out;
+        ball_counter_next = ball_counter;
+        gravity_counter_next = gravity_counter;
+        x_counter_next = x_counter;
+        ball_y_vel_next = ball_y_vel;
+        is_ball_moving_left = 1'b0;
+        ball_speed_next = ball_speed;
+        game_over_next = game_over;
+        score_test_next = score_test;
+        ball_send_trigger_next = 1'b0;
+
+        y_max = upscale ? 479 : 239;
+
+        case (state)
+            IDLE: begin
+                game_over_next  = 0;
+                score_test_next = 0;
+                if (game_start) begin
+                    next = RUNNING_LEFT;
+                end
+            end
+
+            STOP: begin
+                game_over_next = 1;
+                if (game_start) begin
+                    score_test_next = 0;
+                    next = RUNNING_LEFT;
+                end
+            end
+
+            RUNNING_RIGHT: begin
+                game_over_next = 0;
+                if (collision_detected) begin
+                    next = RUNNING_LEFT;
+                    ball_counter_next = 0;
+                    x_counter_next = 0;
+                end else if (ball_x_out >= (upscale ? 640 - 20 : 320 - 20)) begin
+                    next = STOP;
+                    ball_send_trigger_next = 1'b1;
+                end else begin
+                    if (ball_counter >= ball_speed) begin
+                        ball_x_next = ball_x_out + 4;
+                        ball_counter_next = 0;
+
+                        if (gravity_counter == 2'd3) begin
+                            ball_y_vel_next = ball_y_vel + 1;
+                            gravity_counter_next = 0;
+                        end else begin
+                            gravity_counter_next = gravity_counter + 1;
+                        end
+
+                        ball_y_next = ball_y_out + ball_y_vel;
+
+                        if (ball_y_next >= y_max) begin
+                            ball_y_next = y_max;
+                            ball_y_vel_next = -ball_y_vel_next;
+                        end else if (ball_y_next <= y_min) begin
+                            ball_y_next = y_min;
+                            ball_y_vel_next = -ball_y_vel_next;
+                        end
+                    end else begin
+                        ball_counter_next = ball_counter + 1;
+                    end
+                end
+            end
+
+            RUNNING_LEFT: begin
+                is_ball_moving_left = 1'b1;
+                game_over_next = 0;
+
+                if (collision_detected) begin
+                    safe_speed = (estimated_speed < 2) ? 1.6 : estimated_speed;
+                    ball_speed_next = 32'd270000 / safe_speed;
+                end
+
+                if (ball_x_out <= 0) begin
+                    score_test_next = score_test + 1;
+                    next = RUNNING_RIGHT;
+                    ball_counter_next = 0;
+                    x_counter_next = 0;
+                    ball_speed_next = 20'd270000;  // 속도 초기화
+                end else begin
+                    if (ball_counter >= ball_speed) begin
+                        ball_x_next = ball_x_out - 4;
+                        ball_counter_next = 0;
+
+                        if (gravity_counter == 2'd3) begin
+                            ball_y_vel_next = ball_y_vel + 1;
+                            gravity_counter_next = 0;
+                        end else begin
+                            gravity_counter_next = gravity_counter + 1;
+                        end
+
+                        ball_y_next = ball_y_out + ball_y_vel;
+
+                        if (ball_y_next >= y_max) begin
+                            ball_y_next = y_max;
+                            ball_y_vel_next = -ball_y_vel_next;
+                        end else if (ball_y_next <= y_min) begin
+                            ball_y_next = y_min;
+                            ball_y_vel_next = -ball_y_vel_next;
+                        end
+                    end else begin
+                        ball_counter_next = ball_counter + 1;
+                    end
+                end
+            end
+        endcase
+    end
+
+endmodule
